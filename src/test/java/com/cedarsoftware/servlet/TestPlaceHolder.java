5afb8912976007a87d5cefdd8e6839fc6928dc2e
John DeRegnaucourt <jdereg@gmail.com>
2016-08-31 00:36:55 -0400

    - removed place holder test (was there for groovy ecplis compiler)

--- src/test/java/com/cedarsoftware/servlet/TestPlaceHolder.java
+++
@@ -1,9 +0,0 @@
-package com.cedarsoftware.servlet;
-
-/**
- * Delete this file (and the Java folder) when the eclipse groovy compiler no longer
- * needs at least one Java file.
- */
-public class TestPlaceHolder
-{
-}
